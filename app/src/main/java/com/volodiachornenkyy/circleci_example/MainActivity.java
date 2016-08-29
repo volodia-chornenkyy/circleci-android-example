eed9a74264dfa4c73d482e1f5f08a6e58c1e4498
Volodia Chornenkyy <volodia.chornenkyy@techmagic.co>
2016-08-29 10:30:14 +0300

    prepare activity for ui test

--- app/src/main/java/com/volodiachornenkyy/circleci_example/MainActivity.java
+++ app/src/main/java/com/volodiachornenkyy/circleci_example/MainActivity.java
@@ -2,18 +2,26 @@ package com.volodiachornenkyy.circleci_example;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private GreetingCreator greetingCreator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        GreetingCreator greetingCreator = new GreetingCreator();
+        greetingCreator = new GreetingCreator();
 
-        TextView textView = (TextView) findViewById(R.id.text);
-        textView.setText(greetingCreator.get(this, "Volodia"));
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView textView = (TextView) findViewById(R.id.text);
+                textView.setText(greetingCreator.get(view.getContext(), "Volodia"));
+            }
+        });
     }
 }
