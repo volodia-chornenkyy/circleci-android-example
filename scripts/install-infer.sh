2fd3b884c4095a57a528c290dd16817b78c4ac3c
Volodia Chornenkyy <volodia.chornenkyy@techmagic.co>
2016-08-29 17:15:23 +0300

    install opam with apt-get

--- scripts/install-infer.sh
+++ scripts/install-infer.sh
@@ -6,10 +6,12 @@ mkdir -p ~/.infer
 
 if [ ! -e $INFER_HOME/bin ]; then
 
-    wget https://github.com/ocaml/opam/releases/download/1.2.2/opam-1.2.2-x86_64-Linux
-    chmod +x opam-1.2.2-x86_64-Linux
-    sudo cp opam-1.2.2-x86_64-Linux /usr/local/bin/opam
-    opam init --comp=4.02.3
+    # wget https://github.com/ocaml/opam/releases/download/1.2.2/opam-1.2.2-x86_64-Linux
+    # chmod +x opam-1.2.2-x86_64-Linux
+    # sudo cp opam-1.2.2-x86_64-Linux /usr/local/bin/opam
+    # opam init --comp=4.02.3
+
+    apt-get install opam
 
     cd ~/.infer && wget https://github.com/facebook/infer/releases/download/v0.9.2/infer-linux64-v0.9.2.tar.xz
     tar xf infer-linux64-v0.9.2.tar.xz
