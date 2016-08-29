1e84f7da3b9676d694b9203a48a32c810132920f
Volodia Chornenkyy <volodia.chornenkyy@techmagic.co>
2016-08-29 10:10:23 +0300

    refactored something

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-function getAndroidSDK(){
-  export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH"
+export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH"
 
-  DEPS="$ANDROID_HOME/installed-dependencies"
+DEPS="$ANDROID_HOME/installed-dependencies"
 
-  if [ ! -e $DEPS ]; then
+if [ ! -e $DEPS ]; then
     cp -r /usr/local/android-sdk-linux $ANDROID_HOME &&
     echo y | android update sdk -u -a -t android-24 &&
     echo y | android update sdk -u -a -t platform-tools &&
     echo y | android update sdk -u -a -t build-tools-24.0.1 &&
     touch $DEPS
-  fi
-}
+fi
