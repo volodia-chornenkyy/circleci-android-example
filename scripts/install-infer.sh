8c2f31b7889871bee041181ed52e4440bffe377a
Volodia Chornenkyy <volodia.chornenkyy@techmagic.co>
2016-08-29 17:53:36 +0300

    make infer files

--- scripts/install-infer.sh
+++ scripts/install-infer.sh
@@ -6,14 +6,18 @@ mkdir -p ~/.infer
 
 if [ ! -e $INFER_HOME/bin ]; then
 
-    wget https://github.com/ocaml/opam/releases/download/1.2.2/opam-1.2.2-x86_64-Linux
-    chmod +x opam-1.2.2-x86_64-Linux
-    sudo cp opam-1.2.2-x86_64-Linux /usr/local/bin/opam
-    echo y | opam init --comp=4.02.3
+    #wget https://github.com/ocaml/opam/releases/download/1.2.2/opam-1.2.2-x86_64-Linux
+    #chmod +x opam-1.2.2-x86_64-Linux
+    #sudo cp opam-1.2.2-x86_64-Linux /usr/local/bin/opam
+    #echo y | opam init --comp=4.02.3
 
     cd ~/.infer && wget https://github.com/facebook/infer/releases/download/v0.9.2/infer-linux64-v0.9.2.tar.xz
     tar xf infer-linux64-v0.9.2.tar.xz
     rm infer-linux64-v0.9.2.tar.xz
 
-    ./infer-linux64-v0.9.2/build-infer.sh
+    #./infer-linux64-v0.9.2/build-infer.sh
+
+    ./autogen.sh
+    ./configure
+    make java
 fi
