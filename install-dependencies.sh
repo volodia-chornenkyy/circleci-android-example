953f7f0080973b2b2192cef29e6bcde79342824b
Volodia Chornenkyy <volodia.chornenkyy@techmagic.co>
2016-08-29 15:24:26 +0300

    move message inside script

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -5,6 +5,7 @@ export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH"
 DEPS="$ANDROID_HOME/installed-dependencies"
 
 if [ ! -e $DEPS ]; then
+    echo "Downloading Android SDK v24 now!"
     cp -r /usr/local/android-sdk-linux $ANDROID_HOME &&
     echo y | android update sdk -u -a -t android-24 &&
     echo y | android update sdk -u -a -t platform-tools &&
