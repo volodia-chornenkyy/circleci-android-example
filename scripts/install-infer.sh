09d3abaaaf287405a3d99f71cb3fed9b310da14f
Volodia Chornenkyy <volodia.chornenkyy@techmagic.co>
2016-08-29 16:03:32 +0300

    add separate script to install Infer and move other scripts to folder

---
+++ scripts/install-infer.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export PATH="$INFER_HOME/bin:$PATH"
+
+mkdir -p ~/.infer
+
+if [ ! -e $INFER_HOME/bin ]; then
+    cd ~/.infer && wget https://github.com/facebook/infer/releases/download/v0.9.2/infer-linux64-v0.9.2.tar.xz
+    tar xf infer-linux64-v0.9.2.tar.xz
+    rm infer-linux64-v0.9.2.tar.xz
+fi
