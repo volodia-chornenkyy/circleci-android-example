37381073088fc3b77bc0aa8ff9104f14244bac20
Volodia Chornenkyy <volodia.chornenkyy@techmagic.co>
2016-08-29 09:36:14 +0300

    first try to enable caching

---
+++ enviroementSetup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+function getAndroidSDK(){
+  export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH"
+
+  DEPS="$ANDROID_HOME/installed-dependencies"
+
+  if [ ! -e $DEPS ]; then
+    cp -r /usr/local/android-sdk-linux $ANDROID_HOME &&
+    echo y | android update sdk -u -a -t android-24 &&
+    echo y | android update sdk -u -a -t platform-tools &&
+    echo y | android update sdk -u -a -t build-tools-24.0.1 &&
+    touch $DEPS
+  fi
+}
