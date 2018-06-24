f12e8e97802495641c976354a55d554773b3404b
Volodia Chornenkyy <chornenkyy@gmail.com>
2018-06-24 20:42:18 +0300

    Don't run install_dependencies.sh

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -5,7 +5,7 @@ export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH"
 DEPS="$ANDROID_HOME/installed-dependencies"
 
 if [ ! -e $DEPS ]; then
-    echo "Downloading Android SDK v24 now!"
+    echo "Downloading Android SDK now!"
     cp -r /usr/local/android-sdk-linux $ANDROID_HOME &&
     echo y | android update sdk -u -a -t android-24 &&
     echo y | android update sdk -u -a -t build-tools-27.0.3 &&
